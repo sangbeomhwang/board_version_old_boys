34d303f9049dcb3735a8c0668a8a201b0b7c07f3
sangbeom <hhsb2441@gmail.com>
2022-12-30 10:11:35 +0900

    db.js 비밀번호 재설정 후 기능 테스트

--- repository/db.js
+++ repository/db.js
@@ -5,7 +5,7 @@ const pool = mysql
     host: "127.0.0.1",
     port: "3306",
     user: "root",
-    password: "Apple1984!",
+    password: "Hhsb4114!@",
     database: "old_boys",
     connectionLimit: 5,
   })
